c4a49d4deb09b9b482e61ce71cee9a3645b443d8
desigi <pnantel@gmail.com>
2020-01-13 14:00:19 -0500

    Change code based on new api documentation

--- SABThrottle.py
+++ SABThrottle.py
@@ -1,45 +1,40 @@
 import sys
 import urllib
+import json
 import urllib2
+import requests
 from xml.dom import minidom
 from config import config
 
+
+
 PMS_IP = config['PMS_IP']
 PMS_PORT = config['PMS_PORT']
+PMS_API = config['PMS_API']
 
-SAB_URL = config['SAB_URL']
+SAB_URI = config['SAB_URL']
 SAB_API = config['SAB_API']
 
-PMS_URL = 'http://%s:%s/status/sessions' % (PMS_IP, PMS_PORT)
-SAB_URL = 'http://%s/sabnzbd/api?mode=config&name=speedlimit&apikey=%s&value=' % (SAB_URL, SAB_API)
+PMS_URL = 'http://%s:%s/api/v2?apikey=%s&cmd=get_activity' % (PMS_IP, PMS_PORT, PMS_API)
+SAB_PAUSE = 'http://%s/sabnzbd/api?mode=pause&apikey=%s' % (SAB_URI, SAB_API)
+SAB_RESUME = 'http://%s/sabnzbd/api?mode=resume&apikey=%s' % (SAB_URI, SAB_API)
 
 
 
 def get_active_streams(url):
-	dom = minidom.parse(urllib.urlopen(url))
-	total = dom.getElementsByTagName('MediaContainer')
-	for i in total:
-		return int(i.attributes['size'].value)
-		
+        response = requests.get(url)
+	json_output = json.loads(response.text)
+	return json_output['response']['data']['stream_count']
+
 active = get_active_streams(PMS_URL)
 
-if active < 1:
-	speed = config['NONE']
-	url = SAB_URL+speed
+if active >= '1':
+	url = SAB_PAUSE
+	print url
 	req = urllib2.Request(url)
 	result = urllib2.urlopen(req)
-elif active < 3:
-	speed = config['UNDER_THREE']
-	url = SAB_URL+speed
-	req = urllib2.Request(url)
-	result = urllib2.urlopen(req)
-elif active < 5:
-	speed = config['UNDER_FIVE']
-	url = SAB_URL+speed
-	req = urllib2.Request(url)
-	result = urllib2.urlopen(req)
-elif active < 7:
-	speed = config['UNDER_SEVEN']
-	url = SAB_URL+speed
-	req = urllib2.Request(url)
-	result = urllib2.urlopen(req)
+elif active == '0':
+        url = SAB_RESUME
+	print url
+        req = urllib2.Request(url)
+        result = urllib2.urlopen(req)
